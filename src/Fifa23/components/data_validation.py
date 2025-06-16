09815833e69a636446291645eec145e4bd8fbf21
Jakub Jędrych <kubajedrych100@gmail.com>
2025-06-16 15:38:44 +0200

    Improve schema validation logic in data_validation.py

--- src/Fifa23/components/data_validation.py
+++ src/Fifa23/components/data_validation.py
@@ -1,31 +1,63 @@
 from src.Fifa23.components.data_conversion import splitter
-from src.Fifa23.components.data_transformation import data_transform_df
 from src.Fifa23.utils.common import read_yaml
 from src.Fifa23.constants import SCHEMA_FILE_PATH
+from logs import logger,setup_logging
 import pandas as pd
 
+try:
+    setup_logging()
+    logger.info("Logging setup correctly in %s file",__name__)
+except ImportError as e:
+    print("Logging file was not imported correctly %s", e)
+except Exception as e:
+    print("Error found in setup_logging in %s file %s",__name__, e)
+
+class Validator():
+    def __init__(self):
+        self.X,self.y = splitter(True)
+        self.X_df = pd.DataFrame(self.X)
+        self.schema = read_yaml(SCHEMA_FILE_PATH)["COLUMNS"]
+        self.wrong_column_names = []
+        self.wrong_dtypes = []
+
+    def validate_data(self) -> bool:
+        """
+        Validates the column names and data types of the input DataFrame against the schema defined in `schema.yaml`.
+
+        Checks:
+        - All expected columns are present.
+        - Each column has the expected data type.
+
+        Returns:
+            bool: True if all columns match the schema; False otherwise.
+        """
+       
+        for index,(col,dtype) in enumerate(self.schema.items()):
+
+            if col not in self.X_df.columns:
+
+                invalid_name = f"Column {col} has invalid name {self.X_df.columns[index]}"
+                logger.error("Column %s has invalid name %",col,self.X_df.columns[index])
+                self.wrong_column_names.append(invalid_name)
+                continue
+            if dtype != self.X_df[col].dtype:
+                    wrong_dtype_col = f'Column {col} has invalid data type -- {self.X_df[col].dtype} --  Expected: {dtype}'
+                    logger.error("Column %s has invalid data type -- %s ",col,self.X_df[col].dtype)
+                    self.wrong_dtypes.append(wrong_dtype_col)
+
+        if (len(self.wrong_column_names) | len(self.wrong_dtypes)):
+            print(self.wrong_column_names)
+            print(self.wrong_dtypes)
+            return False
+        return True 
+    
+     
+    
+        
+
+if __name__ == "__main__":
+    obj = Validator()
+    print(obj.validate_data())
 
 
-X,y = splitter(True)
-X_df = pd.DataFrame(X)
-print(X_df.info)
-columns = read_yaml(SCHEMA_FILE_PATH)["COLUMNS"]
-print(columns)
 
-missing_type = [types for types in columns.values() ]
-
-columns = [col for col in X_df.columns]
-z = 0
-if len(missing_type) == len(columns):
-    for i,x in zip(missing_type,columns):
-            if X_df.loc[:,x].dtype == i:
-                print("Hurray")
-                z = z + 1
-            print(X_df.loc[:,x].dtype)
-            print(i)
-            if i[0] == x:
-                print(type(X_df.loc[:,x]))
-                print(i[1])
-                if type(X_df.loc[:,x]) == i[1]:
-                    print("Sucess")
-print(z)
