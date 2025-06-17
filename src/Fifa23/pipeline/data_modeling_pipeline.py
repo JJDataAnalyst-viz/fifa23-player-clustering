9650db95dcf2d5f968c756ae990f8e9b4ebbe03d
Jakub Jędrych <kubajedrych100@gmail.com>
2025-06-17 21:12:25 +0200

    Add data modeling pipeline using XGBoost model

---
+++ src/Fifa23/pipeline/data_modeling_pipeline.py
@@ -0,0 +1,26 @@
+from src.Fifa23.components.data_modeling import XgboostModel
+from logs import logger,setup_logging
+
+try:
+    setup_logging()
+    logger.info("Logging setup correctly in data_conversion_pipeline file")
+except ImportError as e:
+    print("Logging file was not imported correctly %s", e)
+except Exception as e:
+    print("Error found in setup_logging in data_conversion_pipeline file %s",e)
+
+
+STAGE_NAME = "Data Modeling Stage"
+
+class DataModelingPipeline():
+    def __init__(self):
+        self.model = XgboostModel()
+    def initiate_data_modeling(self):
+        return self.model.xgboost_user()
+
+
+if __name__ == "__main__":
+    logger.info(">>>>>>>>>>>>>>>>>{%s}<<<<<<<<<<<<<<",STAGE_NAME)
+    obj = DataModelingPipeline()
+    xgb = obj.initiate_data_modeling()
+    logger.info(">>>>>>>>>>>>>>>> STAGE {%s} COMPLETED <<<<<<<<<<<<<<<<",STAGE_NAME)
